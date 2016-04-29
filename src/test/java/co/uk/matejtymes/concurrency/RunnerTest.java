f2eddad8096ea99ccf40ac78ef274f0433e16454
mtymes <m@m.com>
2016-04-29 04:07:09 +0200

    mtymes: updated RunnerTest

--- src/test/java/co/uk/matejtymes/concurrency/RunnerTest.java
+++ src/test/java/co/uk/matejtymes/concurrency/RunnerTest.java
@@ -2,38 +2,45 @@ package co.uk.matejtymes.concurrency;
 
 import org.junit.Test;
 
-import java.util.concurrent.TimeUnit;
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class RunnerTest {
 
     @Test
     public void shouldWaitTillAllTasksAreFinished() {
 
-        // define number of threads
+        // create runner with 10 threads
+
         Runner runner = Runner.runner(10);
 
-        // submit tasks
-        runner.runTaskIn(5, TimeUnit.SECONDS, () -> System.out.println("!"));
-        runner.runRunnableIn(3, TimeUnit.SECONDS, () -> System.out.println("World"));
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("This is awesome");
+            }
+        };
+
+        // submit tasks - Runnable, Callable or Task (custom Runnable that can throw Exceptions) - each call will return you a Future
+        runner.runIn(2, SECONDS, runnable); // this will run in 2 seconds
+        runner.run(runnable); // this will run immediately
+
+
+        runner.waitTillDone(); // blocks after all tasks are finishined (or failed)
+        // after this you can REUSE the class again
+
+
+        // or submit tasks java8 style
+
+        runner.runTaskIn(5, SECONDS, () -> System.out.println("!"));
+        runner.runRunnableIn(3, SECONDS, () -> System.out.println("World"));
         runner.runCallable(() -> { System.out.println("Hello"); return 0; });
 
-        // wait till they finish
-        runner.waitTillDone();
-
-        System.out.println("First set of tasks finished");
-
-
-        // and then REUSE the class again
-
-
-        runner.runTaskIn(2, TimeUnit.SECONDS, () -> System.out.println("This is awesome"));
 
         runner.waitTillDone();
 
-        System.out.println("Second set of tasks finished");
 
-
-        // or just kill it - it's up to you
+        // or just kill it once you're done - it's up to you
 
         runner.shutdown();
     }
