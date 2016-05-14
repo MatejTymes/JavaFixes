304fe77fb0d29522c71592804d836cf9ce768087
mtymes <matejtymes@gmail.com>
2016-05-14 19:46:41 +0100

    mtymes: updating DecimalParserTest

--- src/test/java/mtymes/javafixes/beta/decimal/DecimalParserTest.java
+++ src/test/java/mtymes/javafixes/beta/decimal/DecimalParserTest.java
@@ -1,9 +1,11 @@
 package mtymes.javafixes.beta.decimal;
 
 import mtymes.javafixes.beta.decimal.Decimal.LongDecimal;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 
 import static mtymes.javafixes.test.Conditions.*;
+import static mtymes.javafixes.test.Random.randomInt;
 import static mtymes.javafixes.test.Random.randomLong;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.instanceOf;
@@ -46,27 +48,33 @@ public class DecimalParserTest {
 
     @Test
     public void shouldParseDecimalStringIntoLong() {
-        shouldFindUnscaledValue("9223372036854775807", Long.MAX_VALUE);
-        shouldFindUnscaledValue("-9223372036854775808", Long.MIN_VALUE);
+        shouldFindUnscaledValueOnStringPermutations("9223372036854775807", Long.MAX_VALUE);
+        shouldFindUnscaledValueOnStringPermutations("-9223372036854775808", Long.MIN_VALUE);
 
-        long value = 0;
-        shouldFindUnscaledValue(Long.toString(value), value);
-
-        value = randomLong(positive(), notDivisibleBy10());
-        shouldFindUnscaledValue(Long.toString(value), value);
+        long value = randomLong(positive(), notDivisibleBy10());
+        shouldFindUnscaledValueOnStringPermutations(Long.toString(value), value);
 
         value = randomLong(negative(), notDivisibleBy10());
-        shouldFindUnscaledValue(Long.toString(value), value);
+        shouldFindUnscaledValueOnStringPermutations(Long.toString(value), value);
+
+        shouldFindUnscaledValueOnStringPermutations("0", 0L);
     }
 
-    private void shouldFindUnscaledValue(String stringValue, long expectedUnscaledValue) {
-        Decimal decimal = DecimalParser.parseString(stringValue);
+    private void shouldFindUnscaledValueOnStringPermutations(String stringValue, long expectedUnscaledValue) {
 
+        Decimal decimal = DecimalParser.parseString(stringValue);
         assertThat(decimal, instanceOf(LongDecimal.class));
         assertThat(decimal.unscaledValue(), equalTo(expectedUnscaledValue));
         assertThat(decimal.scale(), equalTo(0));
 
         // todo: improve with random decimal point, zeros and underscores
+
+        int zerosCount = randomInt(1, 100);
+        String newStringValue = stringValue + StringUtils.repeat("0", zerosCount);
+        decimal = DecimalParser.parseString(newStringValue);
+        assertThat(decimal, instanceOf(LongDecimal.class));
+        assertThat(decimal.unscaledValue(), equalTo(expectedUnscaledValue));
+        assertThat(decimal.scale(), equalTo(expectedUnscaledValue == 0 ? 0 : -zerosCount));
     }
 
 
