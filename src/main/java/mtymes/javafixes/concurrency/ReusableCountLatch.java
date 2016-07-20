f4dd4e10ac7eebd08c7c1f8300d3990948a26d06
mtymes <matejtymes@gmail.com>
2016-07-20 22:37:42 +0200

    mtymes: finished ReusableCountLatch javadocs

--- src/main/java/mtymes/javafixes/concurrency/ReusableCountLatch.java
+++ src/main/java/mtymes/javafixes/concurrency/ReusableCountLatch.java
@@ -4,10 +4,35 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.AbstractQueuedSynchronizer;
 
 /**
+ * A synchronization aid that allows one or more threads to wait until
+ * a set of operations being performed in other threads completes.
+ *
+ * <p>A {@code ReusableCountLatch} is initialized with a given <em>count</em>.
+ * The {@link #waitTillZero} methods block until the current count reaches
+ * zero due to invocations of the {@link #decrement} method, after which
+ * all waiting threads are released. If zero has been reached any subsequent
+ * invocations of {@link #waitTillZero} return immediately.
+ * The coun cen be increased calling the {@link #increment()} method and
+ * any subsequent thread calling the {@link #waitTillZero} method will be block
+ * again until another zero is reached.
+ *
+ * <p>{@code ReusableCountLatch} provides more versatility than
+ * {@link java.util.concurrent.CountDownLatch CountDownLatch} as the count
+ * doesn't have to be known upfront and you can reuse this class as many times
+ * as you want to.
+ * It is also better than a {@link java.util.concurrent.Phaser Phaser} whose count
+ * is limited to 65_535. {@code ReusableCountLatch} instead can count up to
+ * 2_147_483_647 (2^31-1).
+ *
+ * <p>Great use case for {@code ReusableCountLatch} is when you wait for tasks on
+ * other threads to finish, but these tasks could trigger more tasks and it is
+ * not know upfront how many will be triggered in total. To simplify this scenario even further
+ * take a look at our implementations of {@link mtymes.javafixes.concurrency.Runner Runner}
+ * of {@link mtymes.javafixes.concurrency.MonitoringTaskSubmitter}.
+ *
  * @author mtymes
  * @since 07/10/16 00:10 AM
  */
-// todo: javadoc
 public class ReusableCountLatch {
 
     private final Sync sync;
@@ -28,7 +53,7 @@ public class ReusableCountLatch {
     }
 
     /**
-     * Constructs a {@code ReusableCountLatch} with initialCount set to 0.
+     * Constructs a {@code ReusableCountLatch} with initial count set to 0.
      */
     public ReusableCountLatch() {
         this(0);
