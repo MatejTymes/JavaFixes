f33a13bdb4667fa5eedcb45a38240595d48a0836
mtymes <matejtymes@gmail.com>
2016-07-24 13:46:57 +0200

    mtymes: updated DataObject javadoc

--- src/main/java/mtymes/javafixes/object/DataObject.java
+++ src/main/java/mtymes/javafixes/object/DataObject.java
@@ -6,9 +6,9 @@ import static org.apache.commons.lang3.builder.ToStringBuilder.reflectionToStrin
 import static org.apache.commons.lang3.builder.ToStringStyle.SHORT_PREFIX_STYLE;
 
 /**
- * {@code DataObject} is intended as a base class for domain objects that adds
- * implementation fo methods {@code equals()}, {@code hashCode()} and {@code toString()}
- * so you don't have to
+ * {@code DataObject} is intended as a base class for domain objects.
+ * It adds reflection based methods {@code equals()}, {@code hashCode()}
+ * and {@code toString()} so your domain classes can be more readable.
  *
  * @author mtymes
  */
