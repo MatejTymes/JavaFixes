6a36b8bd64809646b6b323f7837b4757926cdde7
mtymes <matejtymes@gmail.com>
2019-06-20 03:08:21 +0200

    mtymes: more javadoc for ShutdownInfo

--- src/main/java/javafixes/concurrency/ShutdownInfo.java
+++ src/main/java/javafixes/concurrency/ShutdownInfo.java
@@ -1,15 +1,17 @@
 package javafixes.concurrency;
 
+import java.util.concurrent.Executor;
+
 /**
  * Allows anyone to figure out if a shutdown has been triggered on an executor.
- * This is useful in case a thread missed a shutdown signal as it can
+ * This is useful in case a thread missed a shutdown signal as it can die when it does a check on this interface.
  *
  * @author mtymes
  */
 public interface ShutdownInfo {
 
     /**
-     * @return provides information if a shutdown signal has been received
+     * @return provides information if a shutdown signal has been send to underlying {@link Executor}
      */
     boolean wasShutdownTriggered();
 }
