8d9f71922c28df7c02a57bd721bc20040e3c0fc6
mtymes <matejtymes@gmail.com>
2019-07-10 21:19:05 +0200

    mtymes: added Lazy value

---
+++ src/main/java/javafixes/object/Lazy.java
@@ -0,0 +1,41 @@
+package javafixes.object;
+
+import java.util.concurrent.Callable;
+
+// todo: test this
+public class Lazy<T> {
+
+    private T value;
+    private boolean isInitialized;
+
+    private final Callable<T> valueProvider;
+
+    public Lazy(Callable<T> valueProvider) {
+        this.valueProvider = valueProvider;
+    }
+
+    public static <T> Lazy<T> lazy(Callable<T> valueProvider) {
+        return new Lazy<>(valueProvider);
+    }
+
+    public boolean isInitialized() {
+        return isInitialized;
+    }
+
+    public T value() {
+        if (!isInitialized) {
+            synchronized (this) {
+                if (!isInitialized) {
+                    try {
+                        value = valueProvider.call();
+                        isInitialized = true;
+                    } catch (Exception e) {
+                        throw new IllegalStateException("Failed to initialize Lazy value", e);
+                    }
+                }
+            }
+        }
+
+        return value;
+    }
+}
