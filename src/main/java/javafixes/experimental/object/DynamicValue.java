019150322d1241d73cef45d66a6a831607f78080
mtymes <matejtymes@gmail.com>
2020-08-18 00:20:39 +0200

    mtymes: added ability to generate a proxy from DynamicValue

--- src/main/java/javafixes/experimental/object/DynamicValue.java
+++ src/main/java/javafixes/experimental/object/DynamicValue.java
@@ -5,6 +5,8 @@ import javafixes.object.Value;
 import java.util.Optional;
 import java.util.function.Function;
 
+import static java.lang.reflect.Proxy.newProxyInstance;
+
 // todo: add javadoc
 public interface DynamicValue<T> extends Value<T> {
 
@@ -14,5 +16,15 @@ public interface DynamicValue<T> extends Value<T> {
 
     default <T2> DerivedValue<T2, T> map(Function<T, ? extends T2> derivedValueMapper) {
         return new DerivedValue<>(Optional.empty(), this, derivedValueMapper, Optional.empty());
+    }
+
+    default <T> T asProxy(Class<T> proxyInterface) {
+        return (T) newProxyInstance(
+                this.getClass().getClassLoader(),
+                new Class[]{proxyInterface},
+                (proxy, method, args) -> {
+                    return method.invoke(value(), args);
+                }
+        );
     }
 }
