fd5cb48918edb2a3f14b78d3733c53e088d65798
matejtymes <matejtymes@gmail.com>
2023-03-24 14:06:31 +0100

    mtymes: adding GzipCompressingInputStream

---
+++ src/main/java/javafixes/beta/io/GzipCompressingInputStream.java
@@ -0,0 +1,21 @@
+package javafixes.beta.io;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.zip.GZIPOutputStream;
+
+public class GzipCompressingInputStream extends ConvertedInputStream {
+
+    public GzipCompressingInputStream(
+            InputStream sourceInputStream
+    ) throws IOException {
+        super(sourceInputStream, GZIPOutputStream::new);
+    }
+
+    public GzipCompressingInputStream(
+            InputStream sourceInputStream,
+            int outputBufferSize
+    ) throws IOException {
+        super(sourceInputStream, out -> new GZIPOutputStream(out, outputBufferSize));
+    }
+}
