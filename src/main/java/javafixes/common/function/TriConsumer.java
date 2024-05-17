e50dba6092d67b6f7046fa1720e8fdab3a41c64f
mtymes <matejtymes@gmail.com>
2024-05-17 23:52:12 +0100

    mtymes: TriConsumer javadoc

--- src/main/java/javafixes/common/function/TriConsumer.java
+++ src/main/java/javafixes/common/function/TriConsumer.java
@@ -1,8 +1,25 @@
 package javafixes.common.function;
 
-// todo: mtymes - add javadoc
+/**
+ * Represents an operation that accepts three input arguments and returns no result.
+ * This is the three-arity specialization of {@link java.util.function.Consumer}.
+ * Unlike most other functional interfaces, TriConsumer is expected to operate via side-effects.
+ * <p></p>
+ * This is a <a href="package-summary.html">functional interface</a>
+ * whose functional method is {@link #accept(Object, Object, Object)}.
+ * @param <A> the type of the first argument to the operation
+ * @param <B> the type of the second argument to the operation
+ * @param <C> the type of the third argument to the operation
+ * @author mtymes
+ */
 @FunctionalInterface
 public interface TriConsumer<A, B, C> {
 
+    /**
+     * Performs this operation on the given arguments.
+     * @param a the first input argument
+     * @param b the second input argument
+     * @param c the third input argument
+     */
     void accept(A a, B b, C c);
 }
